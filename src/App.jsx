53d1b9a581b3fa7bd1edae764acc9c5581734b3f
Elshater6498 <elshaterm247@gmail.com>
2024-07-30 17:13:11 +0300

    update in the style of project

--- src/App.jsx
+++ src/App.jsx
@@ -4,36 +4,36 @@ function App() {
       <div className="p-5 rounded-lg bg-white">
         <form action="" className="">
           <div>
-            <input type="text" className="" />
+            <input className="border-none outline-none w-14 h-14 text-lg bg-[rgb(91,91,151)] m-0.5" type="text"/>
           </div>
           <div>
-            <input type="button" value="AC" />
-            <input type="button" value="DE" />
-            <input type="button" value="." />
-            <input type="button" value="/" />
+            <input className="border-none outline-none w-14 h-14 text-lg bg-[rgb(91,91,151)] m-0.5" type="button" value="AC" />
+            <input className="border-none outline-none w-14 h-14 text-lg bg-[rgb(91,91,151)] m-0.5" type="button" value="DE" />
+            <input className="border-none outline-none w-14 h-14 text-lg bg-[rgb(91,91,151)] m-0.5" type="button" value="." />
+            <input className="border-none outline-none w-14 h-14 text-lg bg-[rgb(91,91,151)] m-0.5" type="button" value="/" />
           </div>
           <div>
-            <input type="button" value="7" />
-            <input type="button" value="8" />
-            <input type="button" value="9" />
-            <input type="button" value="*" />
+            <input className="border-none outline-none w-14 h-14 text-lg bg-[rgb(91,91,151)] m-0.5" type="button" value="7" />
+            <input className="border-none outline-none w-14 h-14 text-lg bg-[rgb(91,91,151)] m-0.5" type="button" value="8" />
+            <input className="border-none outline-none w-14 h-14 text-lg bg-[rgb(91,91,151)] m-0.5" type="button" value="9" />
+            <input className="border-none outline-none w-14 h-14 text-lg bg-[rgb(91,91,151)] m-0.5" type="button" value="*" />
           </div>
           <div>
-            <input type="button" value="4" />
-            <input type="button" value="5" />
-            <input type="button" value="6" />
-            <input type="button" value="+" />
+            <input className="border-none outline-none w-14 h-14 text-lg bg-[rgb(91,91,151)] m-0.5" type="button" value="4" />
+            <input className="border-none outline-none w-14 h-14 text-lg bg-[rgb(91,91,151)] m-0.5" type="button" value="5" />
+            <input className="border-none outline-none w-14 h-14 text-lg bg-[rgb(91,91,151)] m-0.5" type="button" value="6" />
+            <input className="border-none outline-none w-14 h-14 text-lg bg-[rgb(91,91,151)] m-0.5" type="button" value="+" />
           </div>
           <div>
-            <input type="button" value="1" />
-            <input type="button" value="2" />
-            <input type="button" value="3" />
-            <input type="button" value="-" />
+            <input className="border-none outline-none w-14 h-14 text-lg bg-[rgb(91,91,151)] m-0.5" type="button" value="1" />
+            <input className="border-none outline-none w-14 h-14 text-lg bg-[rgb(91,91,151)] m-0.5" type="button" value="2" />
+            <input className="border-none outline-none w-14 h-14 text-lg bg-[rgb(91,91,151)] m-0.5" type="button" value="3" />
+            <input className="border-none outline-none w-14 h-14 text-lg bg-[rgb(91,91,151)] m-0.5" type="button" value="-" />
           </div>
           <div>
-            <input type="button" value="0" />
-            <input type="button" value="00" />
-            <input type="button" value="=" />
+            <input className="border-none outline-none w-14 h-14 text-lg bg-[rgb(91,91,151)] m-0.5" type="button" value="0" />
+            <input className="border-none outline-none w-14 h-14 text-lg bg-[rgb(91,91,151)] m-0.5" type="button" value="00" />
+            <input className="border-none outline-none w-14 h-14 text-lg bg-[rgb(91,91,151)] m-0.5" type="button" value="=" />
           </div>
         </form>
       </div>
